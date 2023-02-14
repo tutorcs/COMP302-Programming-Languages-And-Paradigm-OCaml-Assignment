7e4bb9d6238fcaac7a46eea06eae811853280b62
tutorcs <tutorcs@163.com>
2023-02-14 12:37:36 +0800

    WeChat cstutorcs COMP302 Programming Languages And Paradigm OCaml Assignment

---
+++ sat.ml
@@ -0,0 +1,33 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+let collect_variables_tests: (formula * Variable_set.t) list = []
+
+(* TODO: Implement the function. *)
+let collect_variables (formula : formula) : Variable_set.t=
+raise Not_implemented
+(** Question 2 *)
+(* TODO: Add test cases.
+*
+let eval_success_tests : ((truth_assignment * formula)
+* bool) list = [7
+(* TODO: Add test cases.
+*)
+let eval failure tests :
+((truth_assignment * formula)
+* exn) list = []
+(* TODO: Implement the function.
+*)
+let eval (state : truth_assignment) (formula : formula) : bool =
+raise Not_implemented
+(** Question 3 *)
+(* TODO: Add test cases.
+*
+let find_satisfying_assignment_tests : (formula * truth_assignment option) list
+(* TODO: Implement the function.
+*)
+let find_satisfying_assignment (formula : formula) : truth_assignment
+=
+raise Not_implemented
+
